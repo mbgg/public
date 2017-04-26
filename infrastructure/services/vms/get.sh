824c451fc72df448f8fc36f5847cfe2608aafcb4
pedro-nonfree <pedro-nonfree@users.noreply.github.com>
2017-04-26 12:20:51 +0200

    update new firewall, start in VM core

--- infrastructure/services/vms/get.sh
+++ infrastructure/services/vms/get.sh
@@ -3,11 +3,11 @@
 scp core:/etc/network/interfaces core/etc/network/
 scp core:/etc/systemd/system/bind9.service core/etc/systemd/system/
 scp -r core:/etc/bind/ core/etc/
-scp core:/etc/firewall core/etc/
+scp core:/etc/firewall4 core/etc/
+scp core:/etc/firewall6 core/etc/
 
 scp rproxy:/etc/network/interfaces rproxy/etc/network/
 scp rproxy:/etc/haproxy/haproxy.cfg rproxy/etc/haproxy/
 scp rproxy:/etc/firewall rproxy/etc/
-scp -r rproxy:/etc/nginx/sites-available rproxy/etc/nginx
 scp -r rproxy:/etc/nginx/sites-enabled rproxy/etc/nginx
 scp -r rproxy:/etc/default/sslh/ rproxy/etc/default/
